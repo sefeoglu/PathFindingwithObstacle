8dd3ad7ebf718f1364b6950fae645c6f4c9a459e
sefeoglu <sefikaefeoglu@gmail.com>
2019-05-01 20:56:52 +0200

    Add new files

---
+++ model/Board.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 20:31:46 2019
+
+@author: sefik
+"""
+import clingo
+
+
+class Board:
+    def __init__(self):
+        
+        
+        
+        
+    def _on_mode(self):
+        
+        
+        
+    def move(self):
+        
+        
+        
+    def won(self):
---
+++ model/Solver.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 20:47:33 2019
+
+@author: sefik
+"""
+import clingo
+
+class Solver:
+    def __init__(self):
+        

c19616c3daf707d02875c7425f32d044caabeeaf
sefeoglu <sefikaefeoglu@gmail.com>
2019-05-03 11:05:02 +0200

    Board init

--- model/Board.py
+++ model/Board.py
@@ -9,11 +9,21 @@ import clingo
 
 class Board:
     def __init__(self):
+        self.size           = 1
+        self.blocked        = set()
+        self.target        = set()
+        self.pos            = dict()
+        self.robot         = [{}]
+        self.moves          = []
+        self.solution       = None
+
+        control_object = clingo.Control()
+        control_object.load("../Encoding/environment.lp")
+        control_object.ground([("base", [])])
+        control_object.solve(on_model=self.__on_model) 
         
-        
-        
-        
-    def _on_mode(self):
+           
+    def _on_model(self):
         
         
         

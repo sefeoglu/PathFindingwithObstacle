39b01d6000fc0bd4990acec5124b0356317d05ce
sefeoglu <sefikaefeoglu@gmail.com>
2019-05-03 11:09:59 +0200

    __on_model add

--- model/Board.py
+++ model/Board.py
@@ -21,14 +21,18 @@ class Board:
         control_object.load("../Encoding/environment.lp")
         control_object.ground([("base", [])])
         control_object.solve(on_model=self.__on_model) 
-        
-           
-    def _on_model(self):
-        
-        
-        
-    def move(self):
-        
-        
-        
-    def won(self):
+    def __on_model(self, m):
+        for atom in m.symbols(atoms=True):
+            if atom.name == "obstacle" and len(atom.arguments) == 2:
+                y_o,x_o= [n.number for n in atom.arguments]
+                self.blocked.add((x_o, y_o))
+            elif atom.name == "field" and len(atom.arguments) == 2:
+                self.size = max(self.size, atom.arguments[0].number)
+            elif atom.name == "goal" and len(atom.arguments) == 2:
+                y_g,x_g= [n.number for n in atom.arguments]
+                self.target.add((x_g, y_g))
+            elif atom.name == "robot" and len(atom.arguments) == 2:
+                y_r,x_r= [n.number for n in atom.arguments]
+                self.target.add((x_r, y_r))
+                
+board = Board()

8d8e56842354479deba163cd0eb3c463318e3e2d
sefeoglu <sefikaefeoglu@gmail.com>
2019-05-09 20:12:33 +0200

    board editing of data type on the init

--- model/Board.py
+++ model/Board.py
@@ -2,18 +2,20 @@
 """
 Created on Wed May  1 20:31:46 2019
 
-@author: sefik
+@author: sefika
 """
 import clingo
 
-
+# =============================================================================
+# Sefika's OOP
+# =============================================================================
 class Board:
     def __init__(self):
-        self.size         = 1
-        self.blocked      = set()
-        self.goal         = set()
-        self.robot        = set()
-        self.field        = set()
+        
+        self.blocked      = []
+        self.goal         = []
+        self.robot        = []
+        self.field        = []
 
         control_object = clingo.Control()
         control_object.load("../Encoding/environment.lp")
@@ -24,14 +26,14 @@ class Board:
         for atom in m.symbols(atoms=True):
             if atom.name == "obstacle" and len(atom.arguments) == 2:
                 y_o,x_o= [n.number for n in atom.arguments]
-                self.blocked.add((x_o, y_o))
+                self.blocked.append((x_o, y_o))
             elif atom.name == "field" and len(atom.arguments) == 2:
                 y_f,x_f= [n.number for n in atom.arguments]
-                self.field.add((x_f, y_f))
+                self.field.append((x_f, y_f))
             elif atom.name == "goal" and len(atom.arguments) == 2:
                 y_g,x_g= [n.number for n in atom.arguments]
-                self.goal.add((x_g, y_g))
+                self.goal.append((x_g, y_g))
             elif atom.name == "robot" and len(atom.arguments) == 2:
                 y_r,x_r= [n.number for n in atom.arguments]
-                self.robot.add((x_r, y_r))
+                self.robot.append((x_r, y_r))
                 

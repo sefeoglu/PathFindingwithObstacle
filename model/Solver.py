02c077e3f9f7485948fc0631a32795da89191131
sefeoglu <sefikaefeoglu@gmail.com>
2019-05-20 20:34:21 +0200

    editing the parameter of solver method

--- model/Solver.py
+++ model/Solver.py
@@ -2,33 +2,39 @@
 """
 Created on Wed May  1 20:47:33 2019
 
-@author: sefik
+@author: sefika
 """
 import clingo
-
+# =============================================================================
+# Sefika's OOP
+# =============================================================================
 class Solver:
 
     def __init__(self):
+        
         self.solution = []
         self.control_object = clingo.Control()
-        self.control_object.add("check", ["k"], "#external query(k).")
-        self.solver()
+        
         
     def make_on_model(self):
         def on_model(m):
             for atom in m.symbols(atoms=True):
                 if atom.name == "at" and len(atom.arguments) == 3:
-                    s, x, y = [(n.number if n.type == clingo.SymbolType.Number else str(n)) for n in atom.arguments]
-                    self.solution.append((s, x, y))
+                    x, y, s = [(n.number if n.type == clingo.SymbolType.Number else str(n)) for n in atom.arguments]
+                    self.solution.append((x, y, s))
             return False
         return on_model
     
-    def solver(self):
-        self.control_object.load("../Encoding/robotknowledge.lp")
-        self.control_object.load("../Encoding/planner.lp")
+    def solver(self,knowledgeFile = "../Encoding/robotknowledge.lp", plannerFile = "../Encoding/planner.lp"):
+        
+        self.control_object.add("check", ["k"], "#external query(k).")
+        self.control_object.load(knowledgeFile)
+        self.control_object.load(plannerFile)
         self.control_object.ground([("base", [])])
         on_model = self.make_on_model()
+
         t = 0
+        self.solution = []
         while True:
             t += 1
             self.control_object.ground([("step", [t])])
@@ -37,5 +43,3 @@ class Solver:
             self.control_object.assign_external(clingo.Function("query", [t]), True)
             if self.control_object.solve(on_model=on_model).satisfiable:
                 break
-            
-            
